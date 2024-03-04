afe715e88b416d777e2d9cac65f69f1986b61501
Juan Pablo Villagrana Rodríguez <39203605@uaz.edu.mx>
2024-03-04 13:33:15 -0600

    Creación de proyecto
    Se creo la base de código del proyecto

---
+++ Deflamingo/Deflamingo/Deflamingo/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Deflamingo.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace Deflamingo.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
